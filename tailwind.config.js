900a4d5f9e3b06a72718d6683606a3f5ba46409f
fserech <serech63gmail.com>
2026-04-05 09:17:43 -0600

    reestructurando todo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts,scss}"
-  ],
   darkMode: 'class',
+  content: [
+    './src/**/*.{html,ts}',
+  ],
   theme: {
     extend: {
       colors: {
@@ -11,13 +11,35 @@ module.exports = {
           DEFAULT: '#00C793',
           dark:    '#009e75',
           light:   '#e6faf5',
+        },
+        brand: {
+          50:  '#f0fdf8',
+          100: '#dcfce9',
+          200: '#bbf7d4',
+          300: '#86efb8',
+          400: '#4ade91',
+          500: '#00C793',
+          600: '#00a87c',
+          700: '#008a65',
+          800: '#006e51',
+          900: '#005a43',
         }
       },
       fontFamily: {
-        sans:  ['Plus Jakarta Sans', 'sans-serif'],
-        mono:  ['JetBrains Mono', 'monospace'],
-      }
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+        mono: ['JetBrains Mono', 'Fira Code', 'monospace'],
+      },
+      animation: {
+        'slide-in': 'slide-in 0.2s ease-out',
+        'scale-in': 'scale-in 0.15s ease-out',
+        'fade-in':  'fade-in 0.2s ease-out',
+      },
+      keyframes: {
+        'slide-in': { from: { opacity: '0', transform: 'translateX(1rem)' }, to: { opacity: '1', transform: 'translateX(0)' } },
+        'scale-in': { from: { opacity: '0', transform: 'scale(0.95)' },      to: { opacity: '1', transform: 'scale(1)' } },
+        'fade-in':  { from: { opacity: '0' },                                to: { opacity: '1' } },
+      },
     },
   },
   plugins: [],
-}
+};
